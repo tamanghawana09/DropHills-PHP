e6489ef09f4815c0c03acc19f1afc31b8d68f9c0
ergophile-geek <hawanatamang@gmail.com>
2024-05-19 23:42:45 +0545

    code: Task completed

---
+++ Tasks/Task1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phone Book</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="nav-container">
+    <h1>Phone Book Management System</h1>
+        <nav>
+            <button>Login</button>
+        </nav>
+    </div>
+</body>
+
+</html>
